b1cb817a6828c4f07ca3c7cdbec154100cb4a8f1
rague90 <guedjibaabdraouf@yahoo.com>
2022-07-02 12:09:24 +0200

    Add all my files

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    return Book::select('id','name','price','description')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    $request->validate ([
+        'name'=> 'required',
+        'price'=>'required',
+        'description'=>'required'
+    ]);
+    Book::create($request->post());
+    return response()->json([
+        'message' => 'Book created successfuly'
+    ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        return response()->json([
+            'message' => $book
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        $request->validate ([
+            'name'=> 'required',
+            'price'=>'required',
+            'description'=>'required'
+        ]);
+        $book->fill($request->post())->update();
+        return response()->json([
+            'message' => 'Book update successfuly'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return response()->json([
+            'message'=>'book deleted successfully'
+        ]);
+    }
+}
